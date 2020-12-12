4b3bebd1b3bbd4446d22e3a2da28c440628737fc
Shi Hao NG <shihao.ng@amadeus.com>
2020-12-12 17:50:03 +0800

    Initial Project Add

---
+++ src/test/java/seehow/spring/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package seehow.spring.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
