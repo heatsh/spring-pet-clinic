73d02b081cf4463fdd143a938d71c2f3b098756b
Shi Hao NG <shihao.ng@amadeus.com>
2021-01-17 00:10:22 +0800

    Add vet index page and controller. Closes #7

---
+++ pet-clinic-web/src/main/java/seehow/spring/petclinic/controllers/VetController.java
@@ -0,0 +1,12 @@
+package seehow.spring.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+    @RequestMapping({"/vet", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
