22e6790d1ba58973c7b7c77e050e5e2d7a085506
Shi Hao NG <shihao.ng@amadeus.com>
2021-01-20 23:17:05 +0800

    Added vet list and updated controller. Closes #15.

--- pet-clinic-web/src/main/java/seehow/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/seehow/spring/petclinic/bootstrap/DataLoader.java
@@ -3,6 +3,7 @@ package seehow.spring.petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import seehow.spring.petclinic.model.Owner;
+import seehow.spring.petclinic.model.Vet;
 import seehow.spring.petclinic.services.VetService;
 import seehow.spring.petclinic.services.OwnerService;
 
@@ -29,5 +30,17 @@ public class DataLoader implements CommandLineRunner {
         owner2.setLastName("Weston");
         owner2.setId(2L);
         ownerService.save(owner2);
+
+        Vet vet1 = new Vet();
+        vet1.setFirstName("Test");
+        vet1.setLastName("1");
+        vet1.setId(1L);
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setFirstName("Test");
+        vet2.setLastName("2");
+        vet2.setId(2L);
+        vetService.save(vet2);
     }
 }
