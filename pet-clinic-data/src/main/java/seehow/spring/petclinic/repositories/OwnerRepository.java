888cc936d2da941c9763ea3594068731d460e8c4
Shi Hao NG <shihao.ng@amadeus.com>
2021-02-03 23:39:42 +0800

    added spring data jpa service for owners. Closes #40

--- pet-clinic-data/src/main/java/seehow/spring/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/seehow/spring/petclinic/repositories/OwnerRepository.java
@@ -3,5 +3,6 @@ package seehow.spring.petclinic.repositories;
 import org.springframework.data.repository.CrudRepository;
 import seehow.spring.petclinic.model.Owner;
 
-public interface OnwerRepository extends CrudRepository<Owner, Long> {
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
