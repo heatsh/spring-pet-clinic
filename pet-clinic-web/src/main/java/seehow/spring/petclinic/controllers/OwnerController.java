fc3b18d2fd9745d5ecce0be7dcc25115fc46ef86
Shi Hao NG <shihao.ng@amadeus.com>
2021-02-16 16:54:13 +0800

    Display owner record. Closes #53

--- pet-clinic-web/src/main/java/seehow/spring/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/seehow/spring/petclinic/controllers/OwnerController.java
@@ -2,7 +2,11 @@ package seehow.spring.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import seehow.spring.petclinic.model.Owner;
 import seehow.spring.petclinic.services.OwnerService;
 
 @RequestMapping("/owners")
@@ -23,5 +27,18 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners() {
         return "notImplemented";
+    }
+
+    /**
+     * Custom handler for displaying an owner.
+     * @param ownerId the ID of the owner to display
+     * @return a ModelMap with the model attributes for the view
+     */
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        Owner owner = ownerService.findById(ownerId);
+        mav.addObject(owner);
+        return mav;
     }
 }
--- pet-clinic-web/src/test/java/seehow/spring/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/seehow/spring/petclinic/controllers/OwnerControllerTest.java
@@ -14,8 +14,7 @@ import seehow.spring.petclinic.services.OwnerService;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -55,5 +54,13 @@ class OwnerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("notImplemented"));
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+        mockMvc.perform(get("/owners/123")).andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
